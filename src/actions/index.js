ac64f9a068ba75a5105789878a0eb162a0293c38
kartheekmv <you@example.com>
2018-04-09 19:34:02 -0500

    Added charts and corresponding code for that

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 const API_KEY ='1982a29738cb466a9a7281dd282d12bb';
-const ROOT_URL =`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER ='FETCH_WEATHER';
 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url) ;
+    const request = axios.get(url) ; 
     return{
         type: FETCH_WEATHER,
         payload: request
